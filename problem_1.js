5a69894820b5a854cc597e62501025820029eee3
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-03 17:23:01 +0600

    problem 1 solved

--- problem_1.js
+++ problem_1.js
@@ -1,7 +1,17 @@
 //Reverse String
 
 var reverseString = function(str){
-    return str;
+    let arr = str.split("");
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    return arr.join("");
 };
 
 console.log(reverseString("hello"));
