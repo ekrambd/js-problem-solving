56b58e6a2e2ee746cf3f93d8e47f3954cf606006
ekrambd <ekramhossainekram28@gmail.com>
2026-05-03 21:33:53 +0600

    problem 4 solved

---
+++ problem_4.js
@@ -0,0 +1,12 @@
+//4. Sum of array
+
+var sum = function(nums){
+    let total = 0;
+    for(let i = 0; i < nums.length; i++)
+    {
+        total+=nums[i];
+    }
+    return total;    
+};
+
+console.log(sum([1,2,3,4]));
