8c3c6ae20d8a963517f5e7bd99873797063f124e
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-03 17:35:31 +0600

    problem 3 solved

---
+++ problem_3.js
@@ -0,0 +1,18 @@
+//3. Remove duplicates from array
+
+var removeDuplicates = function(nums){
+    let k = 1;
+    for(let i = 1; i < nums.length; i++)
+    {
+        if(nums[i] !== nums[i-1]){
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;    
+};
+
+let nums = [1,2,2,3];
+let len = removeDuplicates(nums);
+let result = nums.slice(0,len);
+console.log(result);
