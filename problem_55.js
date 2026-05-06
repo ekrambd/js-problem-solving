88964f9ecb676c5868c3b0011524d9b100d0ef6e
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-06 16:25:37 +0600

    problem 55 solved

---
+++ problem_55.js
@@ -0,0 +1,16 @@
+//55. Group Anagrams (49)
+
+var groupAnagrams = function(strs) {
+    let map = new Map();
+    for(let i = 0; i < strs.length; i++)
+    {
+        let sorted = strs[i].split("").sort().join('');
+        if(!map.has(sorted)){
+            map.set(sorted,[]);
+        }
+        map.get(sorted).push(strs[i]);
+    }
+    return [...map.values()];   
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
