7a9664e266e9afc8fbc5cb2369b7d549d71c3650
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-06 12:35:45 +0600

    problem 37 solved

---
+++ problem_37.js
@@ -0,0 +1,34 @@
+//37. Three Sum
+
+var threeSum = function(nums){
+    nums.sort((a,b)=>a-b);
+    let res = [];
+
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] === nums[i-1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right];
+
+            if(sum < 0){
+                left++;
+            }else if(sum > 0){
+                right--;
+            }else{
+                res.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+                while(left < right && nums[left] === nums[left - 1]) left++;
+                while(left < right && nums[right] === nums[right + 1]) right--;
+            }
+        }
+    }
+
+    return res;    
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([0,1,1]));
