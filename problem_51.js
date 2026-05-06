e5071b06571c77dbe6ef91eb7cf2e454b06d46e5
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-06 13:08:01 +0600

    problem 51 solved

---
+++ problem_51.js
@@ -0,0 +1,18 @@
+//51. Best Time to Buy and Sell Stock (121)
+
+var maxProfit = function(prices){
+    let buy = prices[0];
+    let maxProfit = 0;
+    for(let i = 0; i < prices.length; i++)
+    {
+        if(prices[i] < buy){
+            buy = prices[i];
+        }else{
+            let profit = prices[i] - buy;
+            maxProfit = Math.max(maxProfit,profit);
+        }
+    }
+    return maxProfit;    
+};
+
+console.log(maxProfit([7,1,5,3,6,4]));
