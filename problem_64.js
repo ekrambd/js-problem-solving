b8504ca000f0ac930828d30ba53e8e1e20eccfb3
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-07 13:37:17 +0600

    problem 64 solved

---
+++ problem_64.js
@@ -0,0 +1,19 @@
+//64. Compress string
+
+var compressString = function(str){
+    let result = "";
+    let count = 1;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === str[i+1]){
+            count++;
+        } else {
+            result += str[i] + count;
+            count = 1;
+        }
+    }
+
+    return result;
+};
+
+console.log(compressString("aaabbc"));
