61bbc5e94864da50b4b321ad1b5103e6c5f7e0a0
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-10 13:04:57 +0600

    problem 13 done

--- problem_13.js
+++ problem_13.js
@@ -1,7 +1,19 @@
 //13. Second largest
 
 var secondLargest = function(nums){
-    return nums;
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(nums[i] > largest){
+            secondLargest = largest;
+            largest = nums[i];
+        }else if(nums[i] > secondLargest && nums[i] !== largest){
+            secondLargest = nums[i];
+        }
+    }
+    return secondLargest;    
 };
 
 console.log(secondLargest([10,5,20,8]));
