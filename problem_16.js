2f2b1f2943e5a35e0f13e6d48b5fe70303908843
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-10 13:35:43 +0600

    problem 16 solved

---
+++ problem_16.js
@@ -0,0 +1,20 @@
+//16. FizzBuzz
+
+var fizzBuzz = function(n){
+    for(let i = 1; i <=n; i++)
+    {
+        if(i % 3 == 0  && i % 5 == 0){
+            console.log("FizzBuzz");
+        }else if(i % 3 == 0){
+            console.log("Fizz");
+        }else if(i % 5 == 0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+    }         
+};
+
+fizzBuzz(5);
+
+//console.log(fizzBuzz(5));
